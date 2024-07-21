27cd0a5602a4fc4fc1c875f9197bae191c5f833e
Mac Klinklinkachorn <macklinkachorn@Macs-MacBook-Pro.local>
2024-07-20 18:53:05 -0700

    Initial commit of python sdks

---
+++ trellis/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trellis',  # Name of your package
+    version='0.1.0',  # Version of your package
+    packages=find_packages(),  # Automatically find packages in the directory
+)
